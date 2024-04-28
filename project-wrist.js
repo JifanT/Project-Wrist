ae0e29aaa3601c4f11264dbf6634f4abe31f43b5
Jifan Thottungal <jifan2185@gmail.com>
2024-04-28 13:08:25 +0530

    js for login and signup modal

--- project-wrist.js
+++ project-wrist.js
@@ -20,3 +20,14 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     };
 });
+
+
+document.getElementById("signup-link").addEventListener("click", function() {
+  document.getElementById("login-form").classList.add("hidden");
+  document.getElementById("signup-form").classList.remove("hidden");
+});
+
+document.getElementById("login-link").addEventListener("click", function() {
+  document.getElementById("signup-form").classList.add("hidden");
+  document.getElementById("login-form").classList.remove("hidden");
+});
