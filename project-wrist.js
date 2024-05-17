585f4be8cefb47e0598bf556268b48edc9da743c
Jifan Thottungal <jifan2185@gmail.com>
2024-05-17 13:01:48 +0530

    updated

--- project-wrist.js
+++ project-wrist.js
@@ -1,33 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var openModalBtn = document.getElementById('openModalBtn');
-    var closeModalBtn = document.getElementById('closeModalBtn');
-    var loginModal = document.getElementById('loginModal');
-  
-    // Open the modal when the button is clicked
-    openModalBtn.onclick = function () {
-      loginModal.style.display = 'block';
-    };
-  
-    // Close the modal when the close button is clicked
-    closeModalBtn.onclick = function () {
+  var openModalBtn = document.getElementById('openModalBtn');
+  var closeModalBtn = document.getElementById('closeModalBtn');
+  var loginModal = document.getElementById('loginModal');
+
+  // Open the modal when the button is clicked
+  openModalBtn.onclick = function () {
+    loginModal.style.display = 'block';
+  };
+
+  // Close the modal when the close button is clicked
+  closeModalBtn.onclick = function () {
+    loginModal.style.display = 'none';
+  };
+
+  // Close the modal if the overlay is clicked
+  window.onclick = function (event) {
+
+    if (event.target === loginModal) {
       loginModal.style.display = 'none';
-    };
-  
-    // Close the modal if the overlay is clicked
-    window.onclick = function (event) {
-      if (event.target === loginModal) {
-        loginModal.style.display = 'none';
-      }
-    };
+    }
+  };
 });
 
-
 document.getElementById("signup-link").addEventListener("click", function() {
-  document.getElementById("login-form").classList.add("hidden");
-  document.getElementById("signup-form").classList.remove("hidden");
+document.getElementById("login-form").classList.add("hidden");
+document.getElementById("signup-form").classList.remove("hidden");
 });
 
 document.getElementById("login-link").addEventListener("click", function() {
-  document.getElementById("signup-form").classList.add("hidden");
-  document.getElementById("login-form").classList.remove("hidden");
+document.getElementById("signup-form").classList.add("hidden");
+document.getElementById("login-form").classList.remove("hidden");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+var btnBuyNow3 = document.getElementById('btnBuyNow3');
+var closeMyModal = document.getElementById('closeMyModal');
+var myModal = document.getElementById('myModal');
+
+// Open the modal when the button is clicked
+btnBuyNow3.onclick = function () {
+  myModal.style.display = 'block';
+};
+
+// Close the modal when the close button is clicked
+closeMyModal.onclick = function () {
+  myModal.style.display = 'none';
+};
+
+// Close the modal if the overlay is clicked
+window.onclick = function (event) {
+
+  if (event.target === myModal) {
+    myModal.style.display = 'none';
+  }
+};
 });
